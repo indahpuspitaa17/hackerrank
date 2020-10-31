93ab6808ecfb6497179978bc951f424b035dfa09
Indah Puspita <47053902+indahpuspitaa17@users.noreply.github.com>
2020-10-31 22:26:19 +0700

    Update day-2a.js

--- 10-day-js/day-2a.js
+++ 10-day-js/day-2a.js
@@ -1,4 +1,4 @@
-se strict';
+'use strict';
 
 process.stdin.resume();
 process.stdin.setEncoding('utf-8');
@@ -14,8 +14,8 @@ process.stdin.on('end', _ => {
     inputString = inputString.trim().split('\n').map(string => {
         return string.trim();
     });
-    
-    main();    
+
+    main();
 });
 
 function readLine() {
@@ -27,29 +27,24 @@ function getGrade(score) {
     // Write your code here
     if (score > 25 && score <= 30) {
         grade = 'A';
-    }
-    else if (score > 20 && score <= 25) {
+    } else if (score > 20 && score <= 25) {
         grade = 'B';
-    }
-    else if (score > 15 && score <= 20) {
+    } else if (score > 15 && score <= 20) {
         grade = 'C';
-    }
-    else if (score > 10 && score <= 15) {
+    } else if (score > 10 && score <= 15) {
         grade = 'D';
-    }
-    else if (score > 5 && score <= 10) {
+    } else if (score > 5 && score <= 10) {
         grade = 'E';
-    }
-    else if (score >= 0 && score <= 5) {
+    } else if (score >= 0 && score <= 5) {
         grade = 'F';
     }
-    
+
     return grade;
 }
 
 
 function main() {
     const score = +(readLine());
-    
+
     console.log(getGrade(score));
 }
