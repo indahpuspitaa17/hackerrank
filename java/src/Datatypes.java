40f5ba6a532f0b47995df2d942fb5dffa4a6812d
indahpuspita <indah_indahpuspita@apps.ipb.ac.id>
2021-12-24 10:34:44 +0700

    Initial commit

---
+++ java/src/Datatypes.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Datatypes {
+    public static void main(String []argh)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for(int i=0; i<t; i++)
+        {
+            try
+            {
+                long x = sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                if(x >= -128 && x<=127)System.out.println("* byte");
+                if(x == (short)x) System.out.println("* short");
+                if(x == (int)x)System.out.println("* int");
+                if(x == (long)x)System.out.println("* long");
+            }
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+        }
+    }
+}
---
+++ java/src/Stdin.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Stdin {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        // Complete this line
+        int b = scan.nextInt();
+        // Complete this line
+        int c = scan.nextInt();
+        // scanner.close();
+
+        System.out.println(a);
+        // Complete this line
+        System.out.println(b);
+        // Complete this line
+        System.out.println(c);
+    }
+
+}
